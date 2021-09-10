2c1600dbbfd012186a5d66a335d0d72ea1792b27
Anoushk Kharangate <anoushka1828@gmail.com>
2021-09-10 16:19:18 +0530

    added twepy view

--- twitterauth/views.py
+++ twitterauth/views.py
@@ -1,3 +1,4 @@
+from logging import error
 from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
 from rest_framework import response
@@ -18,7 +19,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 from dj_rest_auth.social_serializers import TwitterLoginSerializer
 from .models import *
 from allauth.socialaccount.models import SocialAccount, SocialToken
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import tweepy
 from dotenv import load_dotenv
@@ -31,35 +32,44 @@ APP_SECRET = os.environ['API_SECRET_KEY']
 auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
 api = tweepy.API(auth)
 # Create your views here.
+
+
 class TwitterLogin(SocialLoginView):
     serializer_class = TwitterLoginSerializer
     adapter_class = TwitterOAuthAdapter
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def health_check(request):
     return Response(status=status.HTTP_200_OK)
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def home(request, *args, **kwargs):
     return redirect('https://kunai-frontend.vercel.app/dashboard')
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_user_details(request, *args, **kwargs):
     # authorization of consumer key and consumer secret
 
-    #allowed_uris = 'http://127.0.0.1:3000/'
+    # allowed_uris = 'http://127.0.0.1:3000/'
     # print(request.user.id)
-    user = api.get_user(request.user.username)
-    # print(user)
+    try:
+        user = api.get_user(request.user.username)
+        # print(user)
 
-    user_details = {}
+        user_details = {}
 
-    user_details['name'] = user.name
-    #user_details['email'] = u.user.email
-    user_details['screen_name'] = user.screen_name
-    user_details['profile_image_url'] = user.profile_image_url
+        user_details['name'] = user.name
+        # user_details['email'] = u.user.email
+        user_details['screen_name'] = user.screen_name
+        user_details['profile_image_url'] = user.profile_image_url
 
-    return JsonResponse(user_details, status=status.HTTP_200_OK)
+        return JsonResponse(user_details, status=status.HTTP_200_OK)
+    except tweepy.TweepError as e:
+        print(e)
+        return JsonResponse({'error': 'Invalid username'}, status=status.HTTP_400_BAD_REQUEST, error=e)
