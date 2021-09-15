9a7292bf1ac4a44ef13c4cf3e25e5656aea7fe8f
Anoushk Kharangate <anoushka1828@gmail.com>
2021-09-15 10:59:10 +0530

    added save to serializer

--- twitterauth/models.py
+++ twitterauth/models.py
@@ -9,11 +9,5 @@ class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     USERNAME_FIELD = 'username'
 
-    def save(self, **kwargs):
-        if kwargs.has_key('request') and self.user is None:
-            request = kwargs.pop('request')
-            self.user = request.user
-        super(User, self).save(**kwargs)
-
     def __str__(self):
         return '%s %s %s' % (self.username, self.password, self.email)
--- twitterauth/serializers.py
+++ twitterauth/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(UserCreateSerializer):
     class Meta:
         model = User
         fields = ('username', 'password', 'email',)
+
+    def save(self, *args, **kwargs):
+        if kwargs.has_key('request') and self.user is None:
+            request = kwargs.pop('request')
+            self.user = request.user
+        super(User, self).save(**kwargs)
