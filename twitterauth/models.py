836ae72f6709964cd64c6533de8ceef255ac267c
Anoushk Kharangate <anoushka1828@gmail.com>
2021-09-15 10:30:22 +0530

    request.user

--- twitterauth/models.py
+++ twitterauth/models.py
@@ -2,14 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 from jsonfield import JSONField
+
+
 class User(AbstractUser):
     dbtable = 'twitterauth_user'
-    username=models.CharField(max_length=100,unique=True)
-    USERNAME_FIELD='username'
-    #PASSWORD_FIELD='twitter_key'
-    # password=twitter_key
-    # def get_username(self):
-    #     return self.username
-    
+    username = models.CharField(max_length=100, unique=True)
+    USERNAME_FIELD = 'username'
+
+    def save(self, **kwargs):
+        if kwargs.has_key('request') and self.user is None:
+            request = kwargs.pop('request')
+            self.user = request.user
+        super(User, self).save(**kwargs)
+
     def __str__(self):
-        return '%s %s %s'%(self.username,self.password,self.email)
+        return '%s %s %s' % (self.username, self.password, self.email)
