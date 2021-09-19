313bb04c95cfa9c34e47be0eb0bbb7622ceb747d
Anoushk Kharangate <anoushka1828@gmail.com>
2021-09-19 12:53:17 +0530

    basic

---
+++ src/components/NavbarPrivate.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import ThemeSwitch from './ThemeSwitch'
+
+export default function NavbarPublic() {
+    return (
+      <div className="mt-6 mb-6">
+        <div className="flex justify-between">
+          <div>
+            <Link to="/dashboard">
+              <a>
+                <h1 className="font-bold tracking-tighter text-black dark:text-white text-5xl title-font">
+                  Kunai
+                </h1>
+              </a>
+            </Link>
+          </div>
+  
+          <div>
+            <div>
+              <div className="flex flex-row gap-4">
+                <ThemeSwitch />
+                <Link to="/account">
+                  <a>
+                    <button className="flex gap-2 items-center px-6 py-2 font-semibold text-black dark:text-white transition duration-500 ease-in-out transform hover:bg-gray-200 dark:hover:bg-gray-700 border-2 border-gray-500 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <div>My Account</div>
+                    </button>
+                  </a>
+                </Link>
+                <Link to="/create">
+                  <button className="flex gap-2 items-center px-6 py-2 font-semibold text-black dark:text-white transition duration-500 ease-in-out transform hover:bg-gray-200 dark:hover:bg-gray-700 border-2 border-gray-500 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+                    Create Kit
+                  </button>
+                </Link>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
