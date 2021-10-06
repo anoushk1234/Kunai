9c10a434a9611f78b41ee35f19572aa5207e82a8
Anoushk Kharangate <anoushka1828@gmail.com>
2021-10-06 20:21:48 +0530

    delete on main page

--- skeltz_frontend/src/dashboard.jsx
+++ skeltz_frontend/src/dashboard.jsx
@@ -20,6 +20,7 @@ export async function markdownToHtml(markdown) {
 
 export default function Dashboard({ passData }) {
   const [upvoted, setUpvoted] = useState(false);
+  const [loggeduser, setLoggeduser] = useState("");
   const [category, setCategory] = useState([]);
   const prodURL = "https://kunaikit.herokuapp.com";
   //console.log("url: " + thispersondoesnotexist);
@@ -49,6 +50,15 @@ export default function Dashboard({ passData }) {
   useEffect(() => {
     console.log(category);
   }, [category]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(prodURL + "/auth/userdetails/");
+      setLoggeduser(response.data["user_id"]);
+    }
+    fetchData();
+  }, [data]);
+
   return (
     <div>
       <body>
@@ -341,22 +351,39 @@ export default function Dashboard({ passData }) {
                           <p className="font-semibold text-black text-xl">
                             {JSON.parse(item)["upvotes"].length}
                           </p>
-                          {/* <div>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="h-6 w-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            strokeWidth="2"
-                            d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
-                          />
-                        </svg>
-                      </div> */}
+                          {JSON.parse(item)["user_id"] === loggeduser ? (
+                            <button
+                              type="button"
+                              onClick={() => {
+                                axios
+                                  .get(prodURL + "/api/deletekit/" + index)
+                                  .then(() => {
+                                    window.alert("Kit deleted");
+                                    //window.location.href = prodURL + "#/";
+                                  })
+                                  .catch((err) => {
+                                    window.alert(err);
+                                  });
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 flex"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={2}
+                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                                />
+                              </svg>
+                            </button>
+                          ) : (
+                            <></>
+                          )}
                         </div>
                       </div>
                     </div>
