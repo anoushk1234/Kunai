f82b2c6e9b2837a758ef771c94972f70c7659275
Anoushk Kharangate <anoushka1828@gmail.com>
2021-10-10 20:06:02 +0530

    flex issues

--- skeltz_frontend/src/components/kit.jsx
+++ skeltz_frontend/src/components/kit.jsx
@@ -57,101 +57,101 @@ const Kit = ({ data, setData, prodURL, item, loggeduser }) => {
             </ReactMarkdown>
           </p>
         </div>
-        <div className="flex justify-between">
-          <div className="flex flew-row justify-between ">
-            <div className="relative w-12 h-12 flex justify-evenly">
-              <img
-                className="rounded-full border flex border-gray-100 shadow-sm"
-                src={item["profile_image"]}
-                alt="User Avatar"
-                width={320}
-                height={320}
-              />
-              <h1 className="text-base ml-3 flex font-medium leading-relaxed max-w-prose text-gray-700 ">
-                {item["user"]}
-              </h1>
-            </div>
-            <div
-              className="flex flex-row justify-end"
-              style={{ alignItems: "center" }}
+
+        <div className="flex flew-row justify-between ">
+          <div className="relative w-12 h-12 ">
+            <img
+              className="rounded-full border flex border-gray-100 shadow-sm"
+              src={item["profile_image"]}
+              alt="User Avatar"
+              width={320}
+              height={320}
+            />
+            <h1 className="text-base ml-3 flex font-medium leading-relaxed max-w-prose text-gray-700 ">
+              {item["user"]}
+            </h1>
+          </div>
+
+          <div
+            className="flex flex-row justify-end"
+            style={{ alignItems: "center" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 flex mr-1 ml-4"
+              style={{ alignSelf: "center" }}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 flex mr-1 ml-4"
-                style={{ alignSelf: "center" }}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-                />
-              </svg>
-              {hasupvoted ? (
-                <span className="flex mx-4  rounded-lg w-17 p-2 bg-white border-2  border-black flex-row justify-around">
-                  <button
-                    className="flex"
-                    onClick={() => {
-                      axios.get(prodURL + "/api/up/" + item["id"]).then(() => {
-                        setUpvoted(!upvoted);
-                        setHasupvoted(!hasupvoted);
-                      });
-                    }}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+              />
+            </svg>
+            {hasupvoted ? (
+              <span className="flex mx-4  rounded-lg w-17 p-2 bg-white border-2  border-black flex-row justify-around">
+                <button
+                  className="flex"
+                  onClick={() => {
+                    axios.get(prodURL + "/api/up/" + item["id"]).then(() => {
+                      setUpvoted(!upvoted);
+                      setHasupvoted(!hasupvoted);
+                    });
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 flex"
+                    fill="white"
+                    viewBox="0 0 24 24"
+                    stroke="black"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6 flex"
-                      fill="white"
-                      viewBox="0 0 24 24"
-                      stroke="black"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M5 11l7-7 7 7M5 19l7-7 7 7"
-                      />
-                    </svg>
-                  </button>
-                  <p className="flex font-semibold text-black text-xl">
-                    {upvotes}
-                  </p>
-                </span>
-              ) : (
-                <span className="flex mx-4 bg-black rounded-lg w-17 p-2 border-2 border-black flex-row justify-around">
-                  <button
-                    className="flex"
-                    onClick={() => {
-                      axios.get(prodURL + "/api/up/" + item["id"]).then(() => {
-                        setUpvoted(!upvoted);
-                        setHasupvoted(!hasupvoted);
-                      });
-                    }}
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 11l7-7 7 7M5 19l7-7 7 7"
+                    />
+                  </svg>
+                </button>
+                <p className="flex font-semibold text-black text-xl">
+                  {upvotes}
+                </p>
+              </span>
+            ) : (
+              <span className="flex mx-4 bg-black rounded-lg w-17 p-2 border-2 border-black flex-row justify-around">
+                <button
+                  className="flex"
+                  onClick={() => {
+                    axios.get(prodURL + "/api/up/" + item["id"]).then(() => {
+                      setUpvoted(!upvoted);
+                      setHasupvoted(!hasupvoted);
+                    });
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6 flex"
+                    fill="black"
+                    viewBox="0 0 24 24"
+                    stroke="white"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6 flex"
-                      fill="black"
-                      viewBox="0 0 24 24"
-                      stroke="white"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M5 11l7-7 7 7M5 19l7-7 7 7"
-                      />
-                    </svg>
-                  </button>
-                  <p className="flex font-semibold text-white text-xl">
-                    {upvotes}
-                  </p>
-                </span>
-              )}
-            </div>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 11l7-7 7 7M5 19l7-7 7 7"
+                    />
+                  </svg>
+                </button>
+                <p className="flex font-semibold text-white text-xl">
+                  {upvotes}
+                </p>
+              </span>
+            )}
           </div>
         </div>
       </div>
